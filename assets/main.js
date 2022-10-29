7213798bbfd76fb2f629b66e79747f35b5ed0465
frontcarlos <frontcarlos@outlook.com>
2022-10-29 15:17:38 -0300

    scroll concluido

--- assets/main.js
+++ assets/main.js
@@ -1,26 +1,26 @@
 
 /* ===== SHOW MENU ===== */
 
-const showMenu = (toggleId, navId) =>{
+const showMenu = (toggleId, navId) => {
     const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+        nav = document.getElementById(navId)
 
     // Validate that variables exist
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
+    if (toggle && nav) {
+        toggle.addEventListener('click', () => {
             // We add the show-menu class to the div tag with the nav_menu class
             nav.classList.toggle('show-menu')
         })
     }
 }
 
-showMenu('nav-toggle','nav-menu')
+showMenu('nav-toggle', 'nav-menu')
 
 // ===== REMOVE MENU MOBILE =====
 
 const navLink = document.querySelectorAll('.nav-link')
 
-function linkAction(){
+function linkAction() {
     const navMenu = document.getElementById('nav-menu')
     //When we click on each nav-link, we remove the show-menu class
     navMenu.classList.remove('show-menu')
@@ -30,19 +30,28 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 // ===== SCROLL SECTIONS ACTIVE LINKS =====
 const section = document.querySelectorAll('section[id]')
 
-function scrollActive(){
+function scrollActive() {
     const scrollY = window.pageYOffset
 
-    section.forEach(current=>{
+    section.forEach(current => {
         const sectionHeight = current.offsetHeight
         const sectionTop = current.offsetTop - 50;
         section = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
             document.querySelector('nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
-        }else{
+        } else {
             document.querySelector('nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
         }
     })
 }
-window.addEventListener('scroll', scrollActive) 
+window.addEventListener('scroll', scrollActive)
+
+// ===== SHOW SCROLL TOP =====
+
+function scrollTop() {
+    const scrollTop = document.getElementById('scroll-top');
+    // When the scroll is higher tham 560 viewport height, add the show-scroll class to the a tag with the scroll
+    if (this.scrollY >= 200) scrollTop.classList.add('show-scroll'); else scrollTop.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollTop)
